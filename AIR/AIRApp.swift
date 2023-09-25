a517e5a775c1f263f001947b4718783b9611411b
Riley Pederson <rjp9961@gmail.com>
2023-09-25 21:17:04 +1000

    Initial Commit

---
+++ AIR/AIRApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIRApp.swift
+//  AIR
+//
+//  Created by Riley Pederson on 25/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct AIRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
